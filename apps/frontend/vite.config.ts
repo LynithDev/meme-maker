2fa246c1499a98cf863ae9c09c3bb26b64cbc748
LynithDev <61880709+LynithDev@users.noreply.github.com>
2024-10-25 20:32:03 +0200

    chore: Update packages

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -10,4 +10,11 @@ export default defineConfig({
         }),
         sveltekit(),
     ],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                api: "modern-compiler",
+            },
+        },
+    },
 });
